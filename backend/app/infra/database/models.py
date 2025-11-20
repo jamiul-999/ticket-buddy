a361c49e404678eb8286bc4b38369d38a8a79c34
charlie <jhdhj999@gmail.com>
2025-11-20 19:53:50 +0600

    Move models to infra and fix pylint errors

--- backend/app/infra/database/models.py
+++ backend/app/infra/database/models.py
@@ -1,23 +1,20 @@
+"""Modules providing sqlalchemy classes"""
+from datetime import datetime
 from sqlalchemy import Column, Integer, String, Numeric, Date, DateTime
-from sqlalchemy.sql import func
-from infra.database.connection import Base
+from app.infra.database.connection import Base
 
 class BookingDB(Base):
+    """DB model for booking"""
     __tablename__="bookings"
-    
     id = Column(Integer, primary_key=True, index=True)
     name = Column(String, nullable=False)
     phone = Column(String, nullable=False, index=True)
-    
     bus_provider = Column(String, nullable=False)
     from_district = Column(String, nullable=False)
     to_district = Column(String, nullable=False)
     dropping_point = Column(String, nullable=False)
-    
     price = Column(Numeric(10, 2), nullable=False)
-    
     travel_date = Column(Date, nullable=False)
     travel_time = Column(String, nullable=False)
-    booking_date = Column(DateTime, server_default=func.now())
-    
+    booking_date = Column(DateTime, default=datetime.utcnow)
     status = Column(String, default="confirmed", index=True)
