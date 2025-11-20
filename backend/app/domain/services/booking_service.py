07cb1d5648ae01b0b9aef444ebed10171893d03e
charlie <jhdhj999@gmail.com>
2025-11-20 20:15:50 +0600

    Implement booking service to create, get and cancel bookings

---
+++ backend/app/domain/services/booking_service.py
@@ -0,0 +1,32 @@
+"""Import entities and exceptions"""
+from typing import List
+from app.domain.entities import Booking
+from app.domain.exceptions import BookingNotFound, InvalidBooking
+
+class BookingService:
+    """Create, get and cancel Booking """
+    def __init__(self, booking_repo):
+        self.booking_repo = booking_repo
+
+    def create_booking(self, data: dict) -> Booking:
+        """Create booking"""
+        if not data.get('name') or not data.get('phone'):
+            raise InvalidBooking("Name and phone number are required")
+        if len(data.get('phone', '')) < 11:
+            raise InvalidBooking("Invalid phone number!")
+
+        booking = Booking(**data)
+        return self.booking_repo.save(booking)
+
+    def get_bookings_by_phone(self, phone: str) -> List[Booking]:
+        """Get bookings via phone number"""
+        return self.booking_repo.find_by_phone(phone)
+
+    def cancel_booking(self, booking_id: int) -> Booking:
+        """Cancel a booking"""
+        booking = self.booking_repo.find_by_id(booking_id)
+        if not booking:
+            raise BookingNotFound(f"Booking {booking_id} not found")
+
+        booking.cancel()
+        return self.booking_repo.update(booking)
