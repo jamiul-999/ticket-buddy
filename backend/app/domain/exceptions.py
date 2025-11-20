a97d2287a097d1bd560481c75a7a5f9f267bb7db
charlie <jhdhj999@gmail.com>
2025-11-21 01:47:52 +0600

    Remove irrelevant code

--- backend/app/domain/exceptions.py
+++ backend/app/domain/exceptions.py
@@ -56,19 +56,3 @@ class DuplicateBooking(BookingException):
                 "bus_provider": bus_provider
             }
         )
-
-# class RouteNotFound(Exception):
-#     """No routes found"""
-#     pass
-
-# class ProviderNotFound(Exception):
-#     """Provider not found"""
-#     pass
-
-# class DistrictNotFound(Exception):
-#     """District not found"""
-#     pass
-
-# class PhoneNumberValidationError(Exception):
-#     """Invalid phone number format"""
-#     pass
