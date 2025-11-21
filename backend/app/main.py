e5cf50a3cc87e7cde824162d32d406721eccc754
charlie <jhdhj999@gmail.com>
2025-11-22 02:52:19 +0600

    Add CORS support

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,12 +1,22 @@
 """Main file"""
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from app.infra.database.connection import engine
 from app.infra.database import models
 from app.api.routes import bookings, search
 
 app = FastAPI()
 
+
 models.Base.metadata.create_all(engine)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(bookings.router, prefix="/api")
 app.include_router(search.router, prefix="/api")
