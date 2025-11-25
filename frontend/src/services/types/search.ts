da892a9d68dc30e47486c0a083c27befbeb5fb04
charlie <jhdhj999@gmail.com>
2025-11-25 17:28:19 +0600

    (Frontend) Remove unnecessary interfaces from search type

--- frontend/src/services/types/search.ts
+++ frontend/src/services/types/search.ts
@@ -4,17 +4,11 @@ export interface SearchRequest {
   max_price?: number;
 }
 
-export interface BusRoute {
-  id: number;
+export interface RouteResponse {
   provider: string;
   from_district: string;
   to_district: string;
   dropping_point: string;
   price: number;
-  travel_time?: string;
 }
 
-export interface SearchResponse {
-  routes: BusRoute[];
-  total: number;
-}
