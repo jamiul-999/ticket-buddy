9eab76bc810cb27d2ca9cefb58cca27907e46d71
charlie <jhdhj999@gmail.com>
2025-11-29 22:55:53 +0600

    (Frontend) Remove js files to recreate using next js

--- frontend/src/services/api/ragApi.ts
+++
@@ -1,9 +0,0 @@
-import { apiClient } from './client';
-import { QueryRequest, QueryResponse } from '../types/query';
-
-export const ragApi = {
-  query: async (request: QueryRequest): Promise<QueryResponse> => {
-    const response = await apiClient.post<QueryResponse>('/api/query', request);
-    return response.data;
-  },
-};
