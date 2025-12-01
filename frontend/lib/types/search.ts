0721a1a11647a8b95825ef266ea8e105b4062d2a
charlie <jhdhj999@gmail.com>
2025-12-01 20:50:59 +0600

    (Frontend) Add search type

---
+++ frontend/lib/types/search.ts
@@ -0,0 +1,13 @@
+export interface SearchRequest {
+    from_district: string;
+    to_district: string;
+    max_price?: number;
+}
+
+export interface RouteResponse {
+    provider: string;
+    from_district: string;
+    to_district: string;
+    dropping_point: string;
+    price: number;
+}
